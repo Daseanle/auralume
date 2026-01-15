0aa97631b8235e9205b61591db4b17340f90b69c
Daseanle <daseanleelee@gmail.com>
2026-01-15 02:46:40 -0800

    Fix_The_Sun_Card_URL_Verified

--- src/lib/tarot-assets.js
+++ src/lib/tarot-assets.js
@@ -118,7 +118,7 @@ export const MAJOR_ARCANA = [
     {
         "name": "The Sun",
         "number": 19,
-        "image": "https://upload.wikimedia.org/wikipedia/commons/1/17/RWS_Tarot_19_Sun.jpg",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/1/17/RWS_Tarot_19_Sun.jpg", // Verified direct URL
         "keywords": "Positivity, fun, warmth, success, vitality"
     },
     {
